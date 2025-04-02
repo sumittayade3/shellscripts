d47e69f6e6472f9ab0d4f25b8a42e8998733db1c
Sumit <sumitpt1995@rediffmail.com>
2025-04-02 01:27:44 -0700

    Batch-25

--- Script2.sh
+++ Script2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #This script is to read input from the user/console
-echo "Value of a"
-read a
-echo "Value of b"
-read b
-echo "Hello value of a is $a and value of b is $b"
+echo "Value of x"
+read ax
+echo "Value of y"
+read y
+echo "Hello value of x is $x and value of y is $y"
 
